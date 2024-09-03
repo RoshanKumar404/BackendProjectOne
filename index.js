112d11fc754f245f6f23355718d442fbc2b317c9
RoshanKumar404 <roshankuar951@gmail.com>
2024-09-03 23:55:33 +0530

    bhut ku6 karna hia

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const { render } = require('ejs');
 const express= require('express');
 const app= express();
 const path= require('path')
-
+const fs=require('fs');
+const { log } = require('console');
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(express.static(path.join(__dirname ,'public')))
@@ -14,6 +15,10 @@ app.use(express.static(path.join(__dirname ,'public')))
 app.set('view engine', 'ejs')
 
 app.get('/',function(req,res){
+    fs.readdir('./files',function(err,files){
+console.log(files);
+
+    })
     res.render('index')
 });
 app.get('/second/:username',function(req,res){  // i made this route dynamic
