9fb3d8de4180606cbe6800a944816e2799e630b9
RoshanKumar404 <roshankuar951@gmail.com>
2024-09-03 21:36:48 +0530

    made till static routinng

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
+const { render } = require('ejs');
 const express= require('express');
 const app= express();
+const path= require('path')
 
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
+app.use(express.static(path.join(__dirname ,'public')))
+// app.use(function(req,res,next ,){
+
+//     console.log('ku6 to hua par pata nahi');
+//     next();
+// })
+app.set('view engine', 'ejs')
 
 app.get('/',function(req,res){
-    res.send('it worked')
+    res.render('index')
 });
+app.get('/second',function(req,res){
+    res.render('secod')
+})
 
 app.listen(3000 ,function(){
     console.log("its working");
     
 })
+
+
+// const path=require('path');
+// console.log(path.join(__dirname +'/public'));
